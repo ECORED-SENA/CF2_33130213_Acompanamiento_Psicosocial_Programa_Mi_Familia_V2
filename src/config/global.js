beb2899a5f2a0bd31d42b90292e5bb085a58a1c2
elrilacho <camilobr17@gmail.com>
2023-11-08 16:57:17 -0500

    Componente_ajustado

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,19 @@
 export default {
   global: {
-    componenteFormativo: 'Estructuración del acompañamiento psicosocial familiar',
-    descripcionCurso: 'El éxito de un acompañamiento familiar se da principalmente por el notable ejercicio del profesional de acompañamiento familiar (PAF), quien comprende los aspectos de atención y reflexión, la correcta caracterización que realice a través de los instrumentos dispuestos para ello y el buen uso y adaptación de las cartillas metodológicas según el escenario a trabajar.',
+    componenteFormativo:
+      'Estructuración del acompañamiento psicosocial familiar',
+    descripcionCurso:
+      'El éxito de un acompañamiento familiar se da principalmente por el notable ejercicio del profesional de acompañamiento familiar (PAF), quien comprende los aspectos de atención y reflexión, la correcta caracterización que realice a través de los instrumentos dispuestos para ello y el buen uso y adaptación de las cartillas metodológicas según el escenario a trabajar.',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/temas/tema3/a1.png'),
       },
       {
         clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+        imagen: require('@/assets/curso/temas/tema3/a2.png'),
       },
     ],
   },
@@ -31,13 +33,20 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Rol del profesional de acompañamiento familiar',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo:
+              '¿Qué aspectos son relevantes en la comprensión de las familias por parte del PAF, desde los enfoques que considera Mi Familia?',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo:
+              'Aspectos de reflexión y atención constante para el PAF en sus interacciones con las familias.',
+            hash: 't_1_2',
           },
         ],
       },
@@ -45,14 +54,56 @@ export default {
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo:
+          'Proceso de caracterización y diagnóstico del programa Mi Familia',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo:
+              '¿Cuál es el sentido del proceso de caracterización y diagnóstico del programa Mi Familia?',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Ficha de caracterización sociofamiliar',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Genograma',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Mapa de pertenencia',
+            hash: 't_2_4',
+          },
+          {
+            numero: '2.5',
+            titulo: 'Perfil de Vulneratividad y Generatividad',
+            hash: 't_2_5',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Cartillas metodológicas para el acompañamiento familiar',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: '¿Cómo es la estructura de las cartillas metodológicas?',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo:
+              '¿Existen escenarios para flexibilizar la propuesta metodológica que contemplan las cartillas?',
+            hash: 't_3_2',
+          },
+        ],
       },
     ],
     subMenu: [
